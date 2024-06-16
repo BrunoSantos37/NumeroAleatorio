45305519ce18bec0ac97ca5c74f03123eed51bf5
Bruno Candido dos Santos <bc1620143@gmail.com>
2024-06-16 03:45:50 -0300

    Minha versão finalizada

--- app.js
+++ app.js
@@ -13,12 +13,21 @@ const sortear = (quantidadeDeNumeros, minNumeros, maxNumeros) => {
 
      // cria numeros sorteados até for igual a quantidade desejada
      for (let i = 0; i < quantidadeDeNumeros; i++) {
-          //empurra para o array de numerosSorteados cada numero
-          numerosSorteados.push(
-               //condicao para satisfazer a condicao desejada no intervalo
+          //condicao para satisfazer a condicao desejada no intervalo
+          let numeroAleatorio =
                Math.floor(Math.random() * (maxNumeros - minNumeros + 1)) +
-                    minNumeros
-          );
+               minNumeros;
+
+          //loop para não sortear números repetidos
+          while (numerosSorteados.includes(numeroAleatorio)) {
+               // sorteia um novo numero aleatorio caso
+               numeroAleatorio =
+                    Math.floor(Math.random() * (maxNumeros - minNumeros + 1)) +
+                    minNumeros;
+          }
+
+          //empurra para o array de numerosSorteados cada numero
+          numerosSorteados.push(numeroAleatorio);
      }
      //retorna os numeros
      return numerosSorteados;
@@ -33,15 +42,28 @@ const textoResultado = document.querySelector("#resultado");
 
 //quando o botao sorteia for clicado
 botaoSorteia.addEventListener("click", () => {
-     const resultado = sortear(
-          //os valores precisam ser convertidos de strings para numbers
-          parseInt(inputQtdNumeros.value),
-          parseInt(inputMinNumeros.value),
-          parseInt(inputMaxNumeros.value)
-     );
+     //transforma os valores dos inputs strings para inteiros
+     let qtdNumero = parseInt(inputQtdNumeros.value);
+     let deNumero = parseInt(inputMinNumeros.value);
+     let ateNumero = parseInt(inputMaxNumeros.value);
+
+     //guarda a quantidade de numeros possiveis a serem sorteados
+     let qtdNumerosPossiveis = ateNumero - deNumero + 1;
+
+     //verifica se o ate é menor ou igual ao de
+     //verifica se é possível mostrar a quantidade de numeros que foi pedido
+     if (ateNumero <= deNumero || qtdNumero > qtdNumerosPossiveis) {
+          alert(
+               "Algo deu errado. Por favor verifique se os dados estão corretos. Os números sorteados não se repetem."
+          );
+          return;
+     }
+
+     //chama a função com os parametros corretos
+     // const re = ;
 
      // apresenta os numeros na tela
-     textoResultado.innerHTML = ` ${resultado}`;
+     textoResultado.innerHTML = sortear(qtdNumero, deNumero, ateNumero);
 
      //ativa o estilo do botao reiniciar
      botaoReiniciar.classList.remove("container__botao-desabilitado");
@@ -50,15 +72,15 @@ botaoSorteia.addEventListener("click", () => {
 
 //quando o botao reinicia for clicado
 botaoReiniciar.addEventListener("click", () => {
-     //   limpa os campos dos inputs
-     inputs.map((e) => {
-          e.value = "";
-     });
-
      //msg sem numeros quando reiniciar
      textoResultado.innerHTML = "nenhum até agora";
 
      // desabilitar estilo do botao reiniciar
      botaoReiniciar.classList.add("container__botao-desabilitado");
      botaoReiniciar.classList.remove("container__botao");
+
+     //   limpa os campos dos inputs
+     inputs.map((e) => {
+          e.value = "";
+     });
 });
